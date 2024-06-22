04b019a8acad41442abb32c5348c2ed52ec1d742
Purnima <purnima.jogdand@adtran.com>
2024-06-22 11:53:19 +0530

    product list page

--- script.js
+++ script.js
@@ -21,4 +21,40 @@ document.addEventListener("DOMContentLoaded", () => {
       navLinks.classList.remove("active");
     });
   });
+
+  const productList = document.querySelector(".product-list");
+  const productItemTemplate = document.querySelector(".product-item");
+
+  fetch("https://fakestoreapi.com/products")
+    .then((response) => response.json())
+    .then((products) => {
+      // Remove the template product item
+      productItemTemplate.remove();
+
+      products.forEach((product) => {
+        // Clone the template product item
+        const productItem = productItemTemplate.cloneNode(true);
+
+        // Update the placeholders with product data
+        productItem.querySelector(".product-image").src = product.image;
+        // productItem.querySelector(".title").alt = product.title;
+        productItem.querySelector(".title").textContent = product.title;
+        // productItem.querySelector(".price").textContent = `$${product.price}`;
+        // productItem.querySelector(".description").textContent =
+        //   product.description;
+        // productItem.querySelector(".category").textContent = product.category;
+        // productItem.querySelector(
+        //   ".rate"
+        // ).textContent = `⭐ ${product.rating.rate}`;
+        // productItem.querySelector(
+        //   ".count"
+        // ).textContent = `(${product.rating.count} reviews)`;
+
+        // // Append the updated product item to the product list
+        productList.appendChild(productItem);
+      });
+    })
+    .catch((error) => {
+      console.error("Error fetching products:", error);
+    });
 });
